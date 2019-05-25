5ae794c38cfedced17d9af6d5f18c9854e356c65
joan24t <joan_24t@hotmail.com>
2019-05-25 15:31:07 +0200

    New multiselect

--- sia_sports_agency/models.py
+++ sia_sports_agency/models.py
@@ -19,6 +19,99 @@ SOCIAL_NETWORK_CHOICES = (
 )
 
 """
+    Model Extremidad dominante
+"""
+class Extremidad_dominante(models.Model):
+
+    # Attributes
+    nombre = models.CharField(
+        null=False,
+        max_length=150
+    )
+    codigo = models.CharField(
+        null=False,
+        max_length=10,
+        unique=True
+    )
+
+"""
+    Model Deporte
+"""
+class Deporte(models.Model):
+
+    # Attributes
+    nombre = models.CharField(
+        null=False,
+        max_length=150
+    )
+    path_icono = models.CharField(
+        null=False,
+        max_length=150,
+        unique=True
+    )
+    codigo = models.CharField(
+        null=False,
+        max_length=5,
+        unique=True
+    )
+
+"""
+    Model Extremidad dominante
+"""
+class Tipo_jugador(models.Model):
+
+    # Attributes
+    nombre = models.CharField(
+        null=False,
+        max_length=150
+    )
+    codigo = models.CharField(
+        null=False,
+        max_length=5,
+        unique=True
+    )
+
+"""
+    Model Posicion
+"""
+class Posicion(models.Model):
+
+    # Attributes
+    nombre = models.CharField(
+        null=False,
+        max_length=150
+    )
+    codigo = models.CharField(
+        null=False,
+        max_length=5,
+        unique=True
+    )
+    deporte = models.ForeignKey(
+        Deporte,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+"""
+    Model Pais
+"""
+class Pais(models.Model):
+    nombre = models.CharField(
+        null=False,
+        max_length=150
+    )
+    codigo = models.CharField(
+        null=False,
+        max_length=15,
+        unique=True
+    )
+    path_icono = models.CharField(
+        null=False,
+        max_length=150,
+        unique=True
+    )
+
+"""
     Model Usuario
 """
 class Usuario(models.Model):
@@ -92,6 +185,50 @@ class Usuario(models.Model):
         null=True,
         unique=True
     )
+    extremidad = models.ForeignKey(
+        Extremidad_dominante,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    deporte = models.ForeignKey(
+        Deporte,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    tipo = models.ForeignKey(
+        Tipo_jugador,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    paises = models.ManyToManyField(
+        Pais
+    )
+
+"""
+    Model Mensaje
+"""
+class Mensaje(models.Model):
+
+    # Attributes
+    asunto = models.CharField(
+        null=False,
+        max_length=200
+    )
+    cuerpo = models.CharField(
+        null=False,
+        max_length=500
+    )
+    remitente = models.ForeignKey(
+        Usuario,
+        related_name='%(class)s_remitente_id',
+        on_delete=models.CASCADE
+
+    )
+    destinatario = models.ForeignKey(
+        Usuario,
+        related_name='%(class)s_destinatario_id',
+        on_delete=models.CASCADE
+    )
 
 """
     Model Red_social
@@ -107,6 +244,26 @@ class Red_social(models.Model):
     enlace = models.CharField(
         null=False,
         max_length=150
+    )
+    usuario = models.ForeignKey(
+        Usuario,
+        on_delete=models.CASCADE
+    )
+
+"""
+    Model Video
+"""
+class Video(models.Model):
+
+    # Attributes
+    nombre = models.CharField(
+        null=False,
+        max_length=150
+    )
+    path = models.CharField(
+        null=False,
+        max_length=150,
+        unique=True
     )
     usuario = models.ForeignKey(
         Usuario,
