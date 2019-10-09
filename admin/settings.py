7a7e819bf65255db0675ff01dcd3420fc50d97b4
joan24t <joan_24t@hotmail.com>
2019-10-09 18:21:50 +0200

    Conf base de datos

--- admin/settings.py
+++ admin/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '*@-yg8enwv$_qxo$_rl&fip@6!_zv#4oz28#ix@a75rpbyl97o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '217.76.159.21', '192.168.1.111']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '217.76.159.21']
 
 
 # Application definition
@@ -88,7 +88,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'pqytiRhff4',
         'HOST': 'localhost',
-        'PORT': '5433',
+        'PORT': '5432',
     },
 #    'test': {
 #        'ENGINE': 'django.db.backends.sqlite3',
