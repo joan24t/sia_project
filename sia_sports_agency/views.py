05d8bb7253aea61c2d68a6563056010c492ba2ff
joan24t <joan_24t@hotmail.com>
2019-10-27 17:51:01 +0100

    Fecha activación

--- sia_sports_agency/views.py
+++ sia_sports_agency/views.py
@@ -1691,6 +1691,7 @@ def activate(request, uidb64, token):
         token
     ):
         usuario.es_activo = 1
+        usuario.fecha_activacion = datetime.now
         usuario.save()
         return index(request)
     else:
