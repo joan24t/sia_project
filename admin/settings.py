944ac6e0fa4a4ec9af4f5432f150729203bb9fc8
joan24t <joan_24t@hotmail.com>
2019-10-30 23:17:35 +0100

    Logger file

--- admin/settings.py
+++ admin/settings.py
@@ -11,9 +11,37 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import logging
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+    'standard': {
+            'format' : '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+            },
+    },
+    'handlers': {
+        'default': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs', 'django.log'),
+            'maxBytes': 1024*1024*5,
+            'formatter' : 'standard',
+            'backupCount': 5,
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    }
+}
 
 
 # Quick-start development settings - unsuitable for production
