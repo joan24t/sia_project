d20a9ece7d563389dcce662d8f004d7e4701906c
joan24t <joan_24t@hotmail.com>
2019-11-04 17:35:31 +0100

    Error modal subida video

--- sia_sports_agency/static/js/perfil_completo.js
+++ sia_sports_agency/static/js/perfil_completo.js
@@ -53,6 +53,8 @@ $(document).ready(function(){
     consultaCromos(true);
     /*Disparar input de tipo file para la subida de la img del cromo*/
     triggerImgCromo();
+    /*Abrir dialogo subida video*/
+    abrirDialogoVideo();
     /*Dispara el form del login*/
     triggerLogin();
     /*Dispara el form de subida de video*/
@@ -210,6 +212,16 @@ var triggerLogin = function(){
                     window.location.href = '/perfil';
                 }
             }
+        });
+    });
+}
+
+/* Abrir modal subida video */
+var abrirDialogoVideo = function(){
+    $(".video-add").on("click", function() {
+        $('#uploadModalCenter').modal({
+            backdrop: 'static',
+            keyboard: false
         });
     });
 }
